dfc19302570f3d8f80b4466c535435988f91b1ac
SaidLamghari <said.lmg@gmail.com>
2024-04-21 10:32:24 -0700

    6-number_odd_or_even.html

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,44 +1,27 @@
 #!/usr/bin/python3
 """
-The script to start a Flask web application
-The web application be listening on 0.0.0.0, port 5000
-Storage use for fetching data from the storage engine
-(FileStorage or DBStorage) => from models import storage and storage.all(...)
-After each request it remove the current SQLAlchemy Session:
-    Declare a method to handle @app.teardown_appcontext
-    Call in this method storage.close()
-    Routes:
-    /states_list: display a HTML page: (inside the tag BODY)
-    H1 tag: for “States”
-    UL tag: with the list of all State objects
-    present in DBStorage sorted by name (A->Z) tip
-    LI tag:  one State: <state.id>: <B><state.name></B>
-Autor : Said LAMGHARI
+let's get get started with Flask
 """
+
 from flask import Flask, render_template
-from models import storage
-from models.state import State
+from models import *
 
 
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
-def dsply_stateslist():
-    """
-    Dsplay State objects
-    """
+def states_list():
+    """ shows listed states sorted """
     states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def rmv_teardowndb(exception):
-    """
-    Rmove SQLAlchemy Session
-    """
-    storage.close()
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    models.storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
