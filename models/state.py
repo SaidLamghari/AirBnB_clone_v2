a3ce393f2afcab693912ab6b3bacbd7d68cdfc4e
SaidLamghari <said.lmg@gmail.com>
2024-04-21 20:30:59 +0100

    models/state.py

--- models/state.py
+++ models/state.py
@@ -7,36 +7,29 @@ import os
 import models
 from models.city import City
 
-
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
 
     name = Column(String(128), nullable=False)
 
+    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            '''
+            modification
+            Returns a list of City instances with
+            state_id = State.id.
+            '''
+
+            c_dict = models.storage.all(City)
+            return [city for city in c_dict.values() if city.state_id == self.id]
+
     def __init__(self, *args, **kwargs):
-        """ Initializes a new State instance """
+        """ Initializes
+        modification
+        a new State instance """
         super().__init__(*args, **kwargs)
         self.name = kwargs.get('name', '')
         if os.getenv('HBNB_TYPE_STORAGE') == 'db':
             self.cities = relationship('City', cascade='all, delete', backref='state')
-        else:
-            @property
-            def cities(self):
-                '''
-                Returns a list of City instances with
-                state_id = State.id.
-                '''
-
-                c_dict = models.storage.all(City)
-                return [city for city in c_dict.values()
-                        if city.state_id == self.id]
-
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        @property
-        def cities(self):
-        """
-        Method to return the list
-        City objects linked to the current State
-        """
-        return [city for city in models.storage.all(City).values() if city.state_id == self.id]
